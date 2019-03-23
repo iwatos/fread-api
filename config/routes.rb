92946c5aac2c8fef710362336391c4a072a34595
iwatos <nonbiriactive@gmail.com>
2019-03-23 23:52:48 +0900

    fix:users_controllerの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
   resources :lists
-  get 'login/login'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "users" => "users#index"
-  post 'login/login'
+  resources :users
 end
