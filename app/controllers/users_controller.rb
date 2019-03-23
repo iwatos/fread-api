a6388aa7fbadecb788d32585afff5b6bbfa141af
iwatos <nonbiriactive@gmail.com>
2019-03-23 22:43:13 +0900

    fix:api

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
-
+    include ActionController::HttpAuthentication::Token::ControllerMethods
+    before_action :authenticate
+    def authenticate
+        authenticate_or_request_with_http_token do |token,options|
+            token == ENV.fetch('FREAD_TOKEN')
+        end
+    end
+  
     def index
-        render json: { status: 'SUCCESS' }
+        render json: { status: 'SUCCESS'}
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :lists
+  get 'login/login'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "users" => "users#index"
+  post 'login/login'
 end
