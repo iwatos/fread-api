ace5c90874c0340eb7aeacac236ecda7b523fcbe
iwatos <nonbiriactive@gmail.com>
2019-05-07 21:55:52 +0900

    fix:migrationファイル修正

--- db/migrate/20190507120220_add_column_to_entires.rb
+++ db/migrate/20190507120220_add_column_to_entires.rb
@@ -1,6 +1,6 @@
 class AddColumnToEntires < ActiveRecord::Migration[5.1]
   def change
-    add_column :entries, :author, :tex, after: :url
+    add_column :entries, :author, :text, after: :url
     add_column :entries, :image, :text, after: :url
     add_column :entries, :published, :datetime, after: :url
     remove_column :entries, :thumbnail, :varchar
